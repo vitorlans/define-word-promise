f713dcd7a79557fa9a51778e9b450a38693c65d0
Vitor <vitor_hs@live.com>
2016-09-16 12:21:23 -0300

    change to promise response

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Define\-Word
+# Define\-Word-Promise
 ## An easy\-to\-use defintions and synonyms finder
 
+Based in https://www.npmjs.com/package/define-word 
+
 ### API
-This API is very small. There is only two function and they is synchonous. Feel free to change them to asynchronous by removing the while statement in index.js. You will have to rewrite some other things as well.
+This API is very small. There are only two function and they is synchonous.
 
 #### Define
 ```>> require("define-word").define("word")```
@@ -24,32 +26,24 @@ This API is very small. There is only two function and they is synchonous. Feel 
 This allows for easy use with no hassle. An example word-definer program:
 
     getDefinition (text) {
-        var definit = require("define-word").define(text)
-        var buff = "";
-        buff += text + " is a " + definit.type + "\n"
-        for (var i = 0; i < definit.definitions.length; i++) {
-          buff += i + 1 + ". " + definit.definitions[i] + "\n"
-        }
-        return buff;
+        var defineWord = require("define-word");
+        defineWord.define(text).then(function (res) {
+            return res;
+        }).catch(function(err) {
+                console.log('err',err);
+        });
     }
-    >> getDefintion("word")
-    <<  "word is a noun
-        1. a unit of language, consisting of one or more spoken sounds or their written representation, that functions as a principal carrier of meaning. Words are composed of one or more morphemes and are either the smallest units susceptible of independent use or consist of two or three such units combined under certain linking conditions, as with the loss of primary accent that distinguishes black·bird· from black· bird·. Words are usually separated by spaces in writing, and are distinguished phonologically, as by accent, in many languages.
-        2. (used in combination with the first letter of an offensive or unmentionable word, the first letter being lowercase or uppercase, with or without a following hyphen): My mom married at 20, and she mentions the m-word every time I meet someone she thinks is eligible.See also f-word, n-word.
-        3. words.speech or talk: to express one's emotion in words; Words mean little when action is called for.the text or lyrics of a song as distinguished from the music.contentious or angry speech; a quarrel: We had words and she walked out on me.
-        4. a short talk or conversation: Marston, I'd like a word with you.
-        5. an expression or utterance: a word of warning.
-        6. warrant, assurance, or promise: I give you my word I'll be there.
-        7. news; tidings; information: We received word of his death.
-        8. a verbal signal, as a password, watchword, or countersign.
-        9. an authoritative utterance, or command: His word was law.
-        10. Also called machine word. Computers. a string of bits, characters, or bytes treated as a single entity by a computer, particularly for numeric purposes.
-        11. (initial capital letter). Also called the Word, the Word of God. the Scriptures; the Bible.the Logos.the message of the gospel of Christ.
-        12. a proverb or motto."
         
 ### Synonym Finder
 ```
-    >> require("define-word").synonyms("geography")
+    >> getSynonym (text) {
+            var defineWord = require("define-word");
+            defineWord.synonyms(text).then(function (res) {
+                return res;
+            }).catch(function(err) {
+                 console.log('err',err);
+            });
+        }
     << [ 'topography',
   'geopolitics',
   'earth science',
--- index.js
+++ index.js
@@ -10,58 +10,61 @@ All synonyms from Thesaurus.com
 Happy defining! 
 
 */
+var req = require("request");
+var cher = require("cheerio");
+//var deasync = require("deasync")
+
+function getDefUrl(termname) {
+    return "http://www.dictionary.com/browse/" + termname.toLowerCase().replace(/[^a-z]/gi, "") + "?s=t";
+}
+
+function getSynonUrl(word) {
+    return "http://www.thesaurus.com/browse/" + word.toLowerCase().replace(/[^a-z]/gi, "") + "?s=t";
+}
 
 exports.define = function returnDefObject(term) {
     var defstring;
-    var req = require("request")
-    var cher = require("cheerio")
-    //var deasync = require("deasync")
-    function getUrl(termname) {
-        return "http://www.dictionary.com/browse/" + term.toLowerCase().replace(/[^a-z]/gi, "") + "?s=t";
-    }
-    req(getUrl(term), function(error, resp, body) {
-        defstring = cher.load(body)("#source-luna > div:nth-child(1) > section > div.source-data > div.def-list > section:nth-child(1)").text()
+
+    return new Promise(function (resolve) {
+        var url = getDefUrl(term);
+        req(url, function(error, resp, body) {
+            defstring = cher.load(body)("#source-luna > div:nth-child(1) > section > div.source-data > div.def-list > section:nth-child(1)").text();
+            
+            var badArr = defstring.trim().replace(/\: \n(.+?)(?=\n)/g, "").replace(/\n/g, "").replace(/\r/g, "").replace(/  /gi, "").split(/\d\./gi);
+            function stringIsBad(string) {
+                return (string.substring(string.length - 2, string.length).substring(0, 1) === "." || string.substring(string.length - 2, string.length).substring(0, 1) === "?" || string.substring(string.length - 2, string.length).substring(0, 1) === "!") && isNaN(string.substring(string.length - 2, string.length).substring(1, 1)) === false;
+            }
+            for (var i = 0; i < badArr.length; i++) {
+                if (stringIsBad(badArr[i]) === true) badArr[i] = badArr[i].substring(0, badArr[i].length - 1);
+            }
+            var defs = badArr;
+            var defsobj = {};
+            defsobj['type'] = defs[0];
+            defsobj['definitions'] = [];
+            for (var i = 1; i < defs.length; i++) {
+                defsobj['definitions'].push(defs[i]);
+            }
+            return resolve(defsobj);
+        });
+    
     });
-    while (defstring === undefined) {
-        console.log("loop")
-        deasync.runLoopOnce()
-    }
-    var badArr = defstring.trim().replace(/\: \n(.+?)(?=\n)/g, "").replace(/\n/g, "").replace(/\r/g, "").replace(/  /gi, "").split(/\d\./gi)
-    function stringIsBad(string) {
-        return (string.substring(string.length - 2, string.length).substring(0, 1) === "." || string.substring(string.length - 2, string.length).substring(0, 1) === "?" || string.substring(string.length - 2, string.length).substring(0, 1) === "!") && isNaN(string.substring(string.length - 2, string.length).substring(1, 1)) === false
-    }
-    for (var i = 0; i < badArr.length; i++) {
-        if (stringIsBad(badArr[i]) === true) badArr[i] = badArr[i].substring(0, badArr[i].length - 1)
-    }
-    var defs = badArr
-    var defsobj = {}
-    defsobj['type'] = defs[0]
-    defsobj['definitions'] = []
-    for (var i = 1; i < defs.length; i++) {
-        defsobj['definitions'].push(defs[i])
-    }
-    return defsobj;
-}
+};
+
 exports.synonyms = function synonyms(word) {
     var synstring;
-    var req = require("request")
-    var cher = require("cheerio")
-    var deasync = require("deasync")
-    function getUrl() {
-        return "http://www.thesaurus.com/browse/" + word.toLowerCase().replace(/[^a-z]/gi, "") + "?s=t";
-    }
-    req(getUrl(), function(error, resp, body) {
-        synstring = cher.load(body)('#filters-0 > div.relevancy-block > div.relevancy-list').text()
+
+    return new Promise(function (resolve) {
+        req(getSynonUrl(word), function(error, resp, body) {
+            synstring = cher.load(body)('#filters-0 > div.relevancy-block > div.relevancy-list').text();
+
+            var updatedArray = [];
+            var synarr = synstring.trim().replace(/star/g, "").replace(/  /g, "").replace(/         /g, " ").replace(/ /g, "SPACE+HERE").replace(/\n/g, " ").replace(/\r/g, " ").split(" ");
+            for (var i = 0; i < synarr.length; i++) {
+                if (!(synarr[i] === "SPACE+HERE" || synarr[i] === '')) {
+                    updatedArray.push(synarr[i].replace(/SPACE\+HERE/g, " "));
+                }
+            }
+            return resolve(updatedArray);
+        });
     });
-    while (!synstring) {
-        deasync.runLoopOnce()
-    }
-    var updatedArray = []
-    var synarr = synstring.trim().replace(/star/g, "").replace(/  /g, "").replace(/         /g, " ").replace(/ /g, "SPACE+HERE").replace(/\n/g, " ").replace(/\r/g, " ").split(" ");
-    for (var i = 0; i < synarr.length; i++) {
-        if (!(synarr[i] === "SPACE+HERE" || synarr[i] === '')) {
-            updatedArray.push(synarr[i].replace(/SPACE\+HERE/g, " "))
-        }
-    }
-    return updatedArray;
-}
+};
